367ca1a9a430b2f49ad23d1a69561892489b888d
ALEJANDRO REYES ALBILLAR <el_reyes_95@hotmail.com>
2017-04-04 22:45:48 +0200

    Añadidos modulos para ejecucion con ROS de la panorámica

--- Reyeselda95_ws/src/panoramica/stitch.py
+++ Reyeselda95_ws/src/panoramica/stitch.py
@@ -9,31 +9,70 @@ from pyimagesearch.panorama import Stitcher
 import argparse
 import imutils
 import cv2
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+import rospy
 
 class Panorama:
-	# construct the argument parse and parse the arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-f", "--first", required=True,
-		help="path to the first image")
-	ap.add_argument("-s", "--second", required=True,
-		help="path to the second image")
-	args = vars(ap.parse_args())
 
-	# load the two images and resize them to have a width of 400 pixels
-	# (for faster processing)
-	imageA = cv2.imread(args["first"])
-	imageB = cv2.imread(args["second"])
-	imageA = imutils.resize(imageA, width=400)
-	imageB = imutils.resize(imageB, width=400)
+	def __init__(self):
 
-	# stitch the images together to create a panorama
-	stitcher = Stitcher()
-	(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+	    self.bridge = CvBridge()
+	    self.image_sub = rospy.Subscriber("/robot1/camera/rgb/image_raw",Image,self.imageCallbackIzq)
+	    self.image_sub = rospy.Subscriber("/robot1/camera/rgb/image_raw",Image,self.imageCallbackDer)
 
-	# show the images
-	#cv2.imshow("Image A", imageA)
-	#cv2.imshow("Image B", imageB)
-	cv2.imshow("Keypoint Matches", vis)
-	cv2.imshow("Result", result)
-	cv2.imwrite("panorama.png",result)
-	cv2.waitKey(0)
+
+	    self.ImageIzq = None
+	    self.ImageDer = None
+
+	def imageCallbackIzq(self,data):
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+			self.ImageIzq = cv_image
+		except CvBridgeError as e:
+			print(e)
+
+	def imageCallbackDer(self,data):
+	    try:
+	      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+	      self.ImageDer = cv_image
+	    except CvBridgeError as e:
+	      print(e)
+
+	def run(self):
+		while True:
+			# construct the argument parse and parse the arguments
+			#ap = argparse.ArgumentParser()
+			#ap.add_argument("-f", "--first", required=True,
+			#	help="path to the first image")
+			#ap.add_argument("-s", "--second", required=True,
+			#	help="path to the second image")
+			#args = vars(ap.parse_args())
+
+			# load the two images and resize them to have a width of 400 pixels
+			# (for faster processing)
+			#imageA = cv2.imread(args["first"])
+			#imageB = cv2.imread(args["second"])
+			imageA = imutils.resize(self.ImageIzq, width=400)
+			imageB = imutils.resize(self.ImageDer, width=400)
+
+			# stitch the images together to create a panorama
+			stitcher = Stitcher()
+			(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+
+			# show the images
+			#cv2.imshow("Image A", imageA)
+			#cv2.imshow("Image B", imageB)
+			#cv2.imshow("Keypoint Matches", vis)
+			cv2.imshow("Result", result)
+			#cv2.imwrite("panorama.png",result)
+			cv2.waitKey(0)
+
+def main(args):
+	ic = Panorama()
+	rospy.init_node('Panorama', anonymous=True)
+	ic.run()
+  
+
+if __name__ == '__main__':
+    main(sys.argv)
