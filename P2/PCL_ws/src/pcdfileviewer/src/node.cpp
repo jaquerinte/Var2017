be79d391c8e80eb8edad5bf31622b212878f72a5
Ivan Rodriguez <ivanrodriguezferrandez@gmail.com>
2017-05-05 23:44:08 +0200

    añadidos la documentacion y terminado la estraccion de key points con sift

--- P2/PCL_ws/src/pcdfileviewer/src/node.cpp
+++ P2/PCL_ws/src/pcdfileviewer/src/node.cpp
@@ -8,8 +8,12 @@
 
 #include <pcl/io/pcd_io.h>
 #include <iostream>
-#include <pcl/keypoints/sift_keypoint.h>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/io.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/kdtree/kdtree_flann.h>
 
 
@@ -20,64 +24,48 @@ const float min_contrast = 0.001f;
 
 
 
-void visualize_keypoints (const pcl::PointCloud<pcl::PointXYZRGB>::Ptr points,
-                          const pcl::PointCloud<pcl::PointWithScale>::Ptr keypoints)
-{
-  // Add the points to the vizualizer
-  pcl::visualization::PCLVisualizer viz;
-  viz.addPointCloud (points, "points");
-
-  // Draw each keypoint as a sphere
-  for (size_t i = 0; i < keypoints->size (); ++i)
-  {
-    // Get the point data
-    const pcl::PointWithScale & p = keypoints->points[i];
-
-    // Pick the radius of the sphere *
-    float r = 2 * p.scale;
-    // * Note: the scale is given as the standard deviation of a Gaussian blur, so a
-    //   radius of 2*p.scale is a good illustration of the extent of the keypoint
-
-    // Generate a unique string for each sphere
-    std::stringstream ss ("keypoint");
-    ss << i;
-
-    // Add a sphere at the keypoint
-    viz.addSphere (p, 2*p.scale, 1.0, 0.0, 0.0, ss.str ());
-  }
-
-  // Give control over to the visualizer
-  viz.spin ();
-}
-
 
 void simpleVis ()
 {
+	 pcl::visualization::PCLVisualizer viz; 
 	 pcl::PointCloud<pcl::PointWithScale>::Ptr keypoints (new pcl::PointCloud<pcl::PointWithScale>);
 	 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>); 
-	 if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("./PointCloudCaptures/capture_3.pcd", *cloud) == -1){  //* load the file 
+	 if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("./PointCloudCaptures/capture_8.pcd", *cloud) == -1){  //* load the file 
     		PCL_ERROR ("Couldn't read the pcd file  \n"); 
   		} 
 	// Crea el visualizador
-  	pcl::visualization::CloudViewer viewer ("Cloud Viewer");
+  	//pcl::visualization::CloudViewer viewer ("Cloud Viewer");
+  	
   	pcl::SIFTKeypoint<pcl::PointXYZRGB, pcl::PointWithScale> sift;
+  	// Use  KdTree to perform neighborhood searches
+	sift.setSearchMethod(pcl::search::KdTree<pcl::PointXYZRGB>::Ptr(new pcl::search::KdTree<pcl::PointXYZRGB>)); 
+	sift.setScales (min_scale, n_octaves, n_scales_per_octave);
+	sift.setMinimumContrast (min_contrast);
+	sift.setInputCloud (cloud);
 
-  	//pcl::PointCloud<pcl::PointWithScale> result;
-	//sift.setSearchMethod (pcl::KdTreeFLANN<pcl::PointXYZRGB>::Ptr (new pcl::KdTreeFLANN<pcl::PointXYZRGB>));
-	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>()); 
-	sift.setSearchMethod (tree);
-  	sift.setScales(min_scale, n_octaves, n_scales_per_octave);
- 	sift.setMinimumContrast(min_contrast);
-  	sift.setInputCloud(cloud);
-	sift.compute(keypoints);
+	pcl::PointCloud<pcl::PointWithScale>::Ptr keypoints_out_target(new pcl::PointCloud<pcl::PointWithScale>); 
+    sift.compute(*keypoints_out_target); 
+	pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints_target(new pcl::PointCloud<pcl::PointXYZ>); 
+    pcl::copyPointCloud(*keypoints_out_target, *keypoints_target); 
+
+        //Visualize point cloud and keypoints 
+        
+	viz.addPointCloud(cloud, "target"); 
+    viz.addPointCloud(keypoints_target, "keypoint"); 
+    viz.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 255, 0, 0, "keypoint"); 
+
+
   	//visualizo 
-	while(!viewer.wasStopped())
+	while(!viz.wasStopped())
 	{
 	//mientras no se cierre muestre la nube de puntos 
-	  viewer.showCloud (cloud);
-	  
-	  visualize_keypoints(cloud,keypoints);
+	  //viewer.showCloud (cloud);
+
+        
+        viz.spinOnce (100);
+	  //viewerNormal.showCloud(cloud_normals)
 	//Para el hilo 1000 milisegundos 
+	   
 	  boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
 }
